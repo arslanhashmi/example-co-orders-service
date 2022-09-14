0b15be91d86721888963f0400e3e7ccf1faf6772
muhammadarslan <m.arslanijaz9@gmail.com>
2022-09-14 20:12:50 +0500

    Add API to get service by ID

--- handler.py
+++ handler.py
@@ -1,7 +1,8 @@
 import json
 
-from exampleco.models.database import Session
+from exampleco.models.database import session
 from exampleco.models.database.orders import Order, OrderSchema
+from exampleco.models.database.services import ServiceSchema, Service
 
 
 # pylint: disable=unused-argument
@@ -14,8 +15,25 @@ def get_all_orders(event, context):
     """
 
     orders_schema = OrderSchema(many=True)
-    orders = Session.query(Order).all()
+    orders = session.query(Order).all()
     results = orders_schema.dump(orders)
+
+    response = {"statusCode": 200, "body": json.dumps(results)}
+
+    return response
+
+
+# pylint: disable=unused-argument
+def get_service(event, context):
+    """
+    Retrieves a service given its ID.
+
+    Returns:
+        Returns a service given a service ID from the database.
+    """
+    services_schema = ServiceSchema()
+    service = session.query(Service).get(event["pathParameters"]["id"])
+    results = services_schema.dump(service)
 
     response = {"statusCode": 200, "body": json.dumps(results)}
 
